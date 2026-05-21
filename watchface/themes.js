d93e8d74a63fdf0e8e7244ca05ef0f4f823cb2eb
giovaboy <giovanni.zilli@gmail.com>
2026-05-21 13:40:47 +0000

    Refactor code structure for improved readability and maintainability

--- watchface/themes.js
+++ watchface/themes.js
@@ -1,5 +1,8 @@
 export const backgrounds = [
-  { id: 0, preview: 'bg/prev_black.png', path: 'bg/BG_black.png', name: 'Nero'   },
-  { id: 1, preview: 'bg/prev_white.png', path: 'bg/BG_white.png', name: 'Bianco' },
-  { id: 2, preview: 'bg/prev_grey.png',  path: 'bg/BG_grey.png',  name: 'Grigio' },
+  { id: 0, preview: 'bg/prev_black.png',  path: 'bg/BG_black.png',  name: 'Nero'   },
+  { id: 1, preview: 'bg/prev_grey.png',   path: 'bg/BG_grey.png',   name: 'Grigio' },
+  { id: 2, preview: 'bg/prev_cosmo.png',  path: 'bg/BG_cosmo.png',  name: 'Cosmo'  },
+  { id: 3, preview: 'bg/prev_aurora.png', path: 'bg/BG_aurora.png', name: 'Aurora' },
+  { id: 4, preview: 'bg/prev_braci.png',  path: 'bg/BG_braci.png',  name: 'Braci'  },
+  { id: 5, preview: 'bg/prev_abisso.png', path: 'bg/BG_abisso.png', name: 'Abisso' },
 ]
